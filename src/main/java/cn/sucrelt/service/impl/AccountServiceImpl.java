e138ae20fd3f61702ddb9aef7b1f6ec41f7d9eff
tangtangsama <1462339680@qq.com>
2020-09-23 16:09:40 +0800

    使用动态代理实现业务层的事务控制

--- src/main/java/cn/sucrelt/service/impl/AccountServiceImpl.java
+++ src/main/java/cn/sucrelt/service/impl/AccountServiceImpl.java
@@ -16,140 +16,66 @@ import java.util.List;
 public class AccountServiceImpl implements AccountService {
 
     private AccountDao accountDao;
-    private TransactionManager transactionManager;
-
-    public void setTransactionManager(TransactionManager transactionManager) {
-        this.transactionManager = transactionManager;
-    }
+    // private TransactionManager transactionManager;
+    //
+    // public void setTransactionManager(TransactionManager transactionManager) {
+    //     this.transactionManager = transactionManager;
+    // }
 
     public void setAccountDao(AccountDao accountDao) {
         this.accountDao = accountDao;
     }
 
-
     public List<Account> findAllAccount() {
-        try {
-            //1.开启事务
-            transactionManager.beginTransaction();
-            //2.执行操作
-            List<Account> accounts = accountDao.findAllAccount();
-            //3.提交事务
-            transactionManager.commit();
-            //4.返回结果
-            return accounts;
-        } catch (Exception e) {
-            //5.回滚事务
-            transactionManager.rollback();
-            throw new RuntimeException(e);
-        } finally {
-            //6.释放连接
-            transactionManager.release();
-        }
+        // try {
+        //     //1.开启事务
+        //     transactionManager.beginTransaction();
+        //     //2.执行操作
+        List<Account> accounts = accountDao.findAllAccount();
+        // //3.提交事务
+        // transactionManager.commit();
+        // //4.返回结果
+        return accounts;
+        // } catch (Exception e) {
+        //     //5.回滚事务
+        //     transactionManager.rollback();
+        //     throw new RuntimeException(e);
+        // } finally {
+        //     //6.释放连接
+        //     transactionManager.release();
+        // }
     }
 
     public Account findAccountById(Integer accountId) {
-        try {
-            //1.开启事务
-            transactionManager.beginTransaction();
-            //2.执行操作
-            Account account = accountDao.findAccountById(accountId);
-            //3.提交事务
-            transactionManager.commit();
-            //4.返回结果
-            return account;
-        } catch (Exception e) {
-            //5.回滚事务
-            transactionManager.rollback();
-            throw new RuntimeException(e);
-        } finally {
-            //6.释放连接
-            transactionManager.release();
-        }
+        return accountDao.findAccountById(accountId);
     }
-
 
     public void saveAccount(Account account) {
-        try {
-            //1.开启事务
-            transactionManager.beginTransaction();
-            //2.执行操作
-            accountDao.saveAccount(account);
-            //3.提交事务
-            transactionManager.commit();
-        } catch (Exception e) {
-            //5.回滚事务
-            transactionManager.rollback();
-        } finally {
-            //6.释放连接
-            transactionManager.release();
-        }
+        accountDao.saveAccount(account);
     }
-
 
     public void updateAccount(Account account) {
-        try {
-            //1.开启事务
-            transactionManager.beginTransaction();
-            //2.执行操作
-            accountDao.updateAccount(account);
-            //3.提交事务
-            transactionManager.commit();
-        } catch (Exception e) {
-            //5.回滚事务
-            transactionManager.rollback();
-        } finally {
-            //6.释放连接
-            transactionManager.release();
-        }
+        accountDao.updateAccount(account);
     }
-
 
     public void deleteAccount(Integer acccountId) {
-        try {
-            //1.开启事务
-            transactionManager.beginTransaction();
-            //2.执行操作
-            accountDao.deleteAccount(acccountId);
-            //3.提交事务
-            transactionManager.commit();
-        } catch (Exception e) {
-            //5.回滚事务
-            transactionManager.rollback();
-        } finally {
-            //6.释放连接
-            transactionManager.release();
-        }
+        accountDao.deleteAccount(acccountId);
     }
 
-
     public void transfer(String sourceName, String targetName, Float money) {
-        try {
-            //1.开启事务
-            transactionManager.beginTransaction();
-            //2.执行操作
-            System.out.println("transfer....");
-            //2.1根据名称查询转出账户
-            Account source = accountDao.findAccountByName(sourceName);
-            //2.2根据名称查询转入账户
-            Account target = accountDao.findAccountByName(targetName);
-            //2.3转出账户减钱
-            source.setMoney(source.getMoney() - money);
-            //2.4转入账户加钱
-            target.setMoney(target.getMoney() + money);
-            //2.5更新转出账户
-            accountDao.updateAccount(source);
-            int i = 1 / 0;
-            //2.6更新转入账户
-            accountDao.updateAccount(target);
-            //3.提交事务
-            transactionManager.commit();
-        } catch (Exception e) {
-            //4.回滚事务
-            transactionManager.rollback();
-            e.printStackTrace();
-        } finally {
-            //5.释放连接
-            transactionManager.release();
-        }
+        System.out.println("transfer....");
+        //2.1根据名称查询转出账户
+        Account source = accountDao.findAccountByName(sourceName);
+        //2.2根据名称查询转入账户
+        Account target = accountDao.findAccountByName(targetName);
+        //2.3转出账户减钱
+        source.setMoney(source.getMoney() - money);
+        //2.4转入账户加钱
+        target.setMoney(target.getMoney() + money);
+        //2.5更新转出账户
+        accountDao.updateAccount(source);
+        // int i = 1 / 0;
+        //2.6更新转入账户
+        accountDao.updateAccount(target);
     }
 }
